6a27b20859720da9ac1e2233605ac329ddbfd6d0
Alisha5101 <67797184+Alisha5101@users.noreply.github.com>
2024-07-18 15:06:33 +0000

    conversion of data

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score="33";
+// convert score into number 
+
+console.log(Number(33))// will give 33 
+let num1="33abc";
+// convert num1 into number
+let num2=null
+//console.log(typeof num1)// string
+// console.log(Number(num1))// will give NaN(not a number ) of type Number
+// console.log(Number(true))// return 1
+// console.log(Number(num2))// return 0
+// console.log(Number(undefined)) //return NaN if type Number
+
+//conver into boolean
+let somevalue=1;
+
+console.log(Boolean(somevalue))
+// 1=>true 0=>false ""=>false "alia"=>true
