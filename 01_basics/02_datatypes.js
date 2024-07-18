45c607e1f648575166f9d661c7ac5a1712bec0cc
Alisha5101 <67797184+Alisha5101@users.noreply.github.com>
2024-07-18 14:41:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //treat all JS code as newer version
+
+// primitive Data types
+/* 1.number
+ 2.bigint -for long number
+ 3. string -
+ 4.null -means nothing or empty ,standalone value
+ 5.undefined- let name; will give undefined value
+  6.symbol-unique
+  7.boolean-true/false
+  */
+
+  console.log(typeof null) // will give object which is a javascript bug
+  console.log(typeof undefined)// will give undefined
+  console.log(typeof "")// will give string
