09bedc834369991726eb04969b7738c0d42ed59d
Alisha5101 <67797184+Alisha5101@users.noreply.github.com>
2024-07-19 10:24:23 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -16,3 +16,26 @@ let somevalue=1;
 
 console.log(Boolean(somevalue))
 // 1=>true 0=>false ""=>false "alia"=>true
+
+//************************************ Operations ************************************** */
+
+//  1 + "2" will give 12
+//2 + "2" will give 22
+//1 + 1 +"2" will give 22
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
