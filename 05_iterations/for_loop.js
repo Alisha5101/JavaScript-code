3a9372169324873562542cf30576e2de7518fe51
Alisha5101 <67797184+Alisha5101@users.noreply.github.com>
2024-08-24 14:40:31 +0000

    forloop break and continue keyword

--- 05_iterations/for_loop.js
+++ 05_iterations/for_loop.js
@@ -3,20 +3,43 @@ let arr=[]
 for (let index = 0; index < 10; index++) {
    count = index;
    arr.push(count)
-    console.log(count)//print every value
+    // console.log(count)//print every value
     
 }
 
 // console.log(count)//stores last value
-console.log(arr)
+// console.log(arr)
 
 // print table from 2 to 10
 
 for(let i=2;i<=10;i++){
-    console.log(`table of ${i}`)
+    // console.log(`table of ${i}`)
     for(let j=1;j<=10;j++){
         
-        console.log(`${i} * ${j} = ${i*j}`)
+        // console.log(`${i} * ${j} = ${i*j}`)
     }
 }
 
+// break and continue
+
+// break:if we encounter with break keyword we will come out of loop
+
+for (let index = 0; index < 10; index++) {
+    if(index==6){
+        console.log("please stop here")
+        break;
+    }
+   console.log(`value of ${index}`);
+
+    
+}
+
+// continue:The continue statement skip one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+
+for (let index = 0; index < 10; index++) {
+    if(index==6){
+        console.log("please stop here")
+        continue;
+    }
+   console.log(`value of ${index}`);
+}
