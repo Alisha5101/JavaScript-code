ca2ba35d6dd6d60e58ce5cf5b694b8158e8593b5
Alisha5101 <67797184+Alisha5101@users.noreply.github.com>
2024-08-24 14:53:59 +0000

    while and do while loop

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,13 @@
+// while loop:it is known as entry control loop
+let num=0
+// while(num<10){
+// console.log(num);
+// num++
+// }
+
+// do while loop-it is know as exit control loop,it will execute atleast once befor the condiotion checking
+
+do{
+console.log(num);
+num++
+}while(num<20);
