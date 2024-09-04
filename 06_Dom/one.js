0b4ab6aaa3f1e9c554c5a62c2509326f8ed26761
Alisha5101 <67797184+Alisha5101@users.noreply.github.com>
2024-09-04 04:18:16 +0000

    dom

---
+++ 06_Dom/one.js
@@ -0,0 +1,8 @@
+let input=document.getElementById('title')
+console.log(input)
+console.log(document.getElementById('title').id)
+console.log(document.getElementById('title').className)
+
+// getAttribute() and setAttribute()
+
+console.log(document.getElementById('title').setAttribute('class','main_heading'))
